3f8066aa671eb607f9b954560628178c1da274d9
Sebastian Dehn <Sifa@Sebastians-MacBook-Pro.local>
2016-04-10 23:02:27 +0200

    v1 release

---
+++ test/lib/taskContent.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var request = require('request');
+
+function taskContentRoute() {
+  var taskContent = new express.Router();
+  taskContent.use(cors());
+  taskContent.use(bodyParser());
+
+
+  // GET REST endpoint - query params may or may not be populated
+  taskContent.get('/', function(req, res) {
+    console.log(new Date(), 'In taskContent route GET / req.query=', req.query);
+    var options = {
+      method: 'GET',
+      json:true
+    }
+    request(encodeURI(req.query.url), options, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.json({msg: body});
+      } else if (error){
+        res.json({msg: 'Went wrong: ' + error});
+      } else{
+        res.json({msg: 'Status code: ' + response.statusCode});
+      }
+    });
+  });
+
+  // POST REST endpoint - note we use 'body-parser' middleware above to parse the request body in this route.
+  // This can also be added in application.js
+  // See: https://github.com/senchalabs/connect#middleware for a list of Express 4 middleware
+  taskContent.post('/', function(req, res) {
+    
+  });
+
+  return taskContent;
+}
+
+module.exports = taskContentRoute;
